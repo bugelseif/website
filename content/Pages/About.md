45d041f23405c93e317618aeafe28995cc1efc1c
Bruna Ferreira <bferreira.slv@gmail.com>
2023-01-31 00:13:48 -0300

    initial commit

---
+++ content/Pages/About.md
@@ -0,0 +1,8 @@
+Title: Sobre
+Date: 2023-01-05 10:02
+Category: Sobre
+Slug: my-super-post
+
+##Sobre
+
+Olá! Meu nome é Bruna, sou estudante de ciência da computação no IFSC 
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+AUTHOR = 'Bruna Ferreira'
+SITENAME = 'Bruna Ferreira'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+DISPLAY_PAGES_ON_MENU = False
+
+# Blogroll
+# LINKS = (('Github', 'https://github.com/bugelseif'),
+#          )
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/bugelseif'),
+          ('Twitter', 'https://twitter.com/bug_elseif'),
+          ('Dev.to', 'https://dev.to/bug_elseif'),)
+
+DEFAULT_PAGINATION = 3
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = 'C:/Users/Bugs/AppData/Local/Programs/Python/Python311/Lib/site-packages/pelican/themes/bugelseif'
